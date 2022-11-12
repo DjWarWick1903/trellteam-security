cf1c6d146295cc03037ab1403e7ea95556dd61a9
Popescu Robert <robertpopescu1903@gmail.com>
2022-11-13 00:06:46 +0200

    initial commit

---
+++ src/test/java/ro/trellteam/security/TrellteamSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.trellteam.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrellteamSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
